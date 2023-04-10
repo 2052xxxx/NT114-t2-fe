a166173e21887694c610f241fd30a7bb9c07bd62
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-10 22:47:31 +0700

    Update App.jsx

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/header.jsx';
 import SubContent from './components/subContent/subContent';
 import React from 'react';
 import Login from './components/screen/Login/Login';
+import Signup from './components/screen/Signup/Signup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<div> <Header/> <SubContent/></div>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </div>
