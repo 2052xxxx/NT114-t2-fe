dec7af571af9178887e792ed8366804bd5f1220b
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-10 15:04:32 +0700

    Update App.jsx

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/header/header.jsx';
 import SubContent from './components/subContent/subContent';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from './components/login/login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SubContent/>
+      <Router>
+        <Route path="/login" component={Login} />
+      </Router>
     </div>
   );
 }
