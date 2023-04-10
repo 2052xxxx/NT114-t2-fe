24c27f73bd856ec11ba76b6c9de37caabdf98f37
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-10 15:31:17 +0700

    create routes for components

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -3,13 +3,17 @@ import Header from './components/header/header.jsx';
 import SubContent from './components/subContent/subContent';
 import React from 'react';
 import Login from './components/screen/Login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <SubContent/>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<div> <Header/> <SubContent/></div>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
