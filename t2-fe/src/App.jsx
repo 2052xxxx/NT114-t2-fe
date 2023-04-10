93d400186a9dd11b682b2414c0af4008edea9062
Tuanz91 <101273372+Tuanz91@users.noreply.github.com>
2023-04-11 01:23:34 +0700

    change-SignupWE

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -3,11 +3,12 @@ import Header from './components/header/header.jsx';
 import SubContent from './components/subContent/subContent';
 import Login from './components/screen/Login/Login';
 import Signup from './components/screen/Signup/Signup'
+import SignupWE from './components/screen/SignupWE/SignupWE'
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      <SignupWE/>
     </div>
   );
 }
