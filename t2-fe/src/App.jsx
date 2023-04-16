6846816f49b462c98660e9e8002245934390d902
Tuanz91 <101273372+Tuanz91@users.noreply.github.com>
2023-04-17 00:09:21 +0700

    Change bug div

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -8,22 +8,24 @@ import SignupWE from './components/screen/SignupWE/SignupWE';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from './components/screen/MainAfterSignin/Page/Page';
 import HeaderPage from './components/screen/MainAfterSignin/HeaderPage/HeaderPage';
+import CBlog from './components/screen/CBlog/CBlog';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="" element={<div> <Header/> <SubContent/></div>}/>
+          <Route path="" element={<div><Header/><SubContent/></div>}/>
           <Route path="/Login" element={<Login/>}/>
           <Route path="/Signup" element={<Signup/>}/>
-          <Route path="/SignupWE" element={<SignupWE/>}/>     
-          {/* <Route path="/Page" element={<Page/>}/>
-          <Routes path="/HeaderPage" element={<HeaderPage/>}/> */}
+          <Route path="/SignupWE" element={<SignupWE/>}/>
+          <Route path="" element={<div><HeaderPage/></div>}/>   
+          
+          
         </Routes>
+        
       </BrowserRouter>
       
-
     </div>
   );
 }
