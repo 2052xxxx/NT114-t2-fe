5d411b70b52484f1b5f5d5234f1acf5b06addc69
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-26 14:27:56 +0700

    Update Writing.jsx

--- t2-fe/src/components/screen/Writing/Writing.jsx
+++ t2-fe/src/components/screen/Writing/Writing.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import HeaderPage from '../MainAfterSignin/HeaderPage/HeaderPage';
 
 export default function Writing(){
     return(
         <div>
-            <span>Title</span>
-            <p>Content</p>
+            <HeaderPage/>
+            <div> 
+                <textarea rows="3" cols="70" placeholder="Title"/>
+            </div>
+            <div>
+                <textarea rows="5" cols="70" placeholder="Content"/>
+            </div>
         </div>
     )
 }
