dad087aa7de56f861f9fb0bdc6a038a5215aae82
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-26 16:50:00 +0700

    responsive /home
    npm install react-bootstrap bootstrap nếu chạy không được

--- t2-fe/src/components/subContent/subContent.jsx
+++ t2-fe/src/components/subContent/subContent.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import './subContent.css'
 import subTheGIF from './animee.gif'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 export default function SubContent(){
     return(
         <div className="subContent">
-            <div className="Content">
-                <h1>WRITE READ WRITE</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Perferendis aliquam error at recusandae autem laboriosam saepe, dolores accusamus reprehenderit. 
-                Aperiam vero libero eligendi incidunt debitis non, repellendus culpa sunt. 
-                Delectus!</p>
-            </div>
-            <div className="sub">
-                <img src={subTheGIF} alt="my Aestyle" />
-            </div>
+            <Container className="container-md">
+                <Row>
+                    <Col className="content">  
+                        <div>
+                            <h1>WRITE READ WRITE</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Perferendis aliquam error at recusandae autem laboriosam saepe, dolores accusamus reprehenderit. 
+                            Aperiam vero libero eligendi incidunt debitis non, repellendus culpa sunt. 
+                            Delectus!</p>  
+                        </div>                                                
+                    </Col>
+                    <Col className="content">        
+                        <img className="myGIF" src={subTheGIF} alt="my Aestyle" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
+        
     )
 }
