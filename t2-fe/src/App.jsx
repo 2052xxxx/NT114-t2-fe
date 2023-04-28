a95aad9639f47801b2017e4c5a35904c2c90a7e7
2052xxxx <20522039@gm.uit.edu.vn>
2023-04-28 12:31:52 +0700

    Update App.jsx

--- t2-fe/src/App.jsx
+++ t2-fe/src/App.jsx
@@ -7,7 +7,9 @@ import Signup from './components/screen/Signup/Signup';
 import SignupWE from './components/screen/SignupWE/SignupWE';
 import HeaderPage from './components/screen/MainAfterSignin/HeaderPage/HeaderPage';
 import CBlog from './components/screen/CBlog/CBlog';
+import CBlogNext from './components/screen/CBlogNext/CBlogNext';
 import Writing from './components/screen/Writing/Writing';
+import Pagez from './components/screen/MainAfterSignin/Pagez/Pagez';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -23,6 +25,8 @@ function App() {
           <Route path="/signupWE" element={<SignupWE/>}/> 
           <Route path="/headerPage" element={<HeaderPage/>}/>
           <Route path="/cBlog" element={<CBlog/>}/>
+          <Route path="/CBlogNext" element={<CBlogNext/>}/>
+          <Route path="/pagez" element={<Pagez/>}/>
           <Route path="/writing" element={<Writing/>}/>
         </Routes>
       </BrowserRouter>
