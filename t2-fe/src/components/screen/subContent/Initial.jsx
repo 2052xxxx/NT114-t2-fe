57eb2a0c2a737ea94684dcb1c5c9a7bcdb182c9e
2052xxxx <20522039@gm.uit.edu.vn>
2023-06-21 12:05:02 +0700

    Initial

--- t2-fe/src/components/screen/subContent/Initial.jsx
+++ t2-fe/src/components/screen/subContent/Initial.jsx
@@ -2,27 +2,33 @@ import React from "react";
 import Header from "../header/header";
 import SubContent from "../subContent/subContent";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Initial(){
     const [tokenCheck, setTokenCheck] = useState(false);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const token = localStorage.getItem('token');
-
+    const responseData = {
+        "username" : username,
+        "email" : email
+    }
     useEffect(() => {
         if (token === null) {
             setTokenCheck(false);
         }
         else {
             const url = 'https://localhost:7015/api/User';
-            fetch(url, {
-                method: 'GET',
+            axios.get(url, {
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `bearer ${token}`,
                 },
             }).then((response) => {
                 if (response.status === 200) {
-                    console.log(response);
                     setTokenCheck(true);
+                    setUsername(response.data.username);
+                    setEmail(response.data.email);
                 }
                 else if (response.status === 401) {
                     setTokenCheck(false);
